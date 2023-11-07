36afbd9322a26666530e9d9bb53513c19952ab22
Kat Raible <raiblekat@gmail.com>
2023-11-06 20:51:47 -0500

    Kat adding

--- conflict_resolution.py
+++ conflict_resolution.py
@@ -2,3 +2,4 @@
 
 print("Once upon a time, there was a land far, far away.")
 print("The land was full of coders and kittens. They had a party that was coming up that they needed to prepare for.")
+print("The party was to celebrate 100 years of peacefull cohabitation between the coders and kittens.")
