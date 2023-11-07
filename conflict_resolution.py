5877b221ccfb0566fccdb12c44444df7fbb9c8a5
Newie Vongmanivanh <vongmanivanh.n9@gmail.com>
2023-11-06 20:49:51 -0500

    Addition of sentence

--- conflict_resolution.py
+++ conflict_resolution.py
@@ -1,4 +1,4 @@
 ## Adding the first sentence to our story
 
 print("Once upon a time, there was a land far, far away.")
-print("The land was full of coders and kittens.")
+print("The land was full of coders and kittens. They had a party that was coming up that they needed to prepare for.")
