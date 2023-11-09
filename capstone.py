dbd3b47e810d9b2a60fc54a8138ebb98f331eed4
Aranda <ajabiro1211@gmail.com>
2023-11-08 21:06:32 -0500

    added payment portion

--- capstone.py
+++ capstone.py
@@ -1,8 +1,13 @@
+from datetime import date
+
+
 class Product:
     def __init__(self, name, category, price):
         self.name = name
         self.category = category
         self.price = price
+
+
 products = [
     Product("Dog", "Mammals", 199.00),
     Product("Cat", "Mammals", 99.00),
@@ -18,22 +23,27 @@ products = [
     Product("Turtle", "Reptiles", 45.00),
 ]
 
+
 def display_menu():
     print("Welcome to the GC Pet Store! Here is what we have to offer: ")
     for i, pet in enumerate(products):
         print(f'{i + 1}. {pet.name}: ${pet.price:.2f}')
 
+
 def sales_tax(total, tax):
-    return total * (tax/100)
+    return total * (tax / 100)
+
 
 order = []
 tax = 6.00
 
 while True:
     display_menu()
-    option = int(input("What pet would you like to adopt? Enter the corresponding number. Type '0' if you are ready to finalize your purchase: "))
-    if 1 <= option and option <=len(products):
-        howmany = int(input("How many would you like? Enter a valid value: "))
+    option = int(input(
+        "What pet would you like to adopt? Enter the corresponding number. Type '0' if you are ready to finalize your "
+        "purchase: "))
+    if 1 <= option and option <= len(products):
+        howmany = int(input(f"How many would you like? Enter a valid value: "))
         selection = products[option - 1]
         print(f"You have selected {howmany} {selection.name} for ${selection.price:.2f}!")
         total = selection.price * howmany
@@ -43,7 +53,7 @@ while True:
 
     elif option == 0:
         print("Here is your total: ")
-        subtotal = sum(item[2]for item in order)
+        subtotal = sum(item[2] for item in order)
         salestax = sales_tax(subtotal, tax)
         grandtotal = subtotal + salestax
 
@@ -51,3 +61,31 @@ while True:
         print(f"Sales Tax: ${salestax:.2f}")
         print(f"Grand Total: ${grandtotal:.2f}")
         break
+
+while True:
+    payment_type = input("Would you like to pay with cash, check, or credit? ")
+    if payment_type == "cash":
+        cash_tender = int(input("Amount tendered: "))
+        change = cash_tender - grandtotal
+        print(f"Change:  ${change}")
+        break
+    elif payment_type == "check":
+        check_num = int(input("Check number: "))
+        break
+    elif payment_type == "credit":
+        cc_num = int(input("Credit card number: "))
+        exp = int(input("Expiration: "))
+        cvv = int(input("CVV: "))
+
+        break
+    else:
+        print("Invalid payment type. Please enter cash, check or credit.")
+
+print("Thank you for shopping at the GC Pet Store!")
+print("Here is you receipt: ")
+print(f'Date: {date.today()}')
+# print all items ordered
+print(f'Subtotal: {subtotal}')
+print(f'Sales Tax: {salestax}')
+print(f'Total: {grandtotal}')
+print(f'Payment Method: {payment_type}')
